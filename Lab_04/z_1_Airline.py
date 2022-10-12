f3366da7a642b08d72d309bd5d691dd6d0cff6a9
konek1x <konexk1x@gmail.com>
2022-10-12 13:41:57 +0300

    Rename z_2.1.py to Lab_02/z_2.1.py

--- Lab_04/z_1_Airline.py
+++ Lab_04/z_1_Airline.py
@@ -3,11 +3,26 @@
 # Создать список объектов. Вывести:
 # a) список рейсов для заданного пункта назначения;
 # б) список рейсов для заданного дня недели;
+import datetime
+
 
 class Airline:
-    def __init__(self, destination, flight_id, plane_type, depart_time, day_of_week):
-        self.destination = destination
-        self.flight_id = flight_id
-        self.plane_type = plane_type
-        self.depart_time = depart_time
-        self.day_of_week = day_of_week
+    def __init__(self, destination, flight_id, plane_type, depart_date, depart_time, day_of_week):
+        try:
+            if depart_date < datetime.datetime.today().strftime("%d.%m.%Y"):
+                raise Exception("Missing data")
+            else:
+                self.destination = destination
+                self.flight_id = flight_id
+                self.plane_type = plane_type
+                self.depart_date = depart_date
+                self.depart_time = depart_time
+                self.day_of_week = day_of_week
+        except:
+            print("Your departure date is incorrect")
+
+
+# import datetime
+# a = datetime.datetime.today().strftime("%d.%m.%Y")
+# print(a)
+# print(type(a))
