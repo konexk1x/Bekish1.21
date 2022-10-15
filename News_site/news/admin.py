dca30b9548b9e96b7786c67826bc2026265eaf5a
konek1x <konexk1x@gmail.com>
2022-10-15 13:03:02 +0300

    Rename z_2.1.py to Lab_02/z_2.1.py

--- News_site/news/admin.py
+++ News_site/news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Link, News, Photo, Message, Mark
+admin.site.register(Link)
+admin.site.register(News)
+admin.site.register(Photo)
+admin.site.register(Message)
+admin.site.register(Mark)
